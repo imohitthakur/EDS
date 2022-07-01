ea48dd025d2075e9c4eaff246dc02d68bac65c06
imohitthakur <imohitthakur@gmail.com>
2022-07-01 11:06:46 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of the rise and recovery of the first COVID pandemic in the 21st century with a focus on the recorded cases as well as records of vaccination of the population in 3 different countries as a part of the coursework in the study of Enterprise Data Science.',
+    author='MRT',
+    license='',
+)
